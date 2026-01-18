c0a6e5359f4f0decd9df360115778a03066d2dd9
tarikozturk017 <tarik.ozturkk1@gmail.com>
2026-01-18 10:58:14 -0500

    Add User model + repository abstraction

---
+++ SecureAccess.Api/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using SecureAccess.Api.Domain;
+
+namespace SecureAccess.Api.Repositories;
+
+
+public interface IUserRepository
+{
+    User? GetByEmail(string email);
+    void Add(User user);
+}
---
+++ SecureAccess.Api/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using SecureAccess.Api.Domain;
+
+namespace SecureAccess.Api.Repositories;
+
+public sealed class InMemoryUserRepository : IUserRepository
+{
+    private readonly Dictionary<string, User> _byEmail =
+        new(StringComparer.OrdinalIgnoreCase);
+
+    public User? GetByEmail(string email)
+        => _byEmail.TryGetValue(email, out var user) ? user : null;
+
+    public void Add(User user)
+        => _byEmail[user.Email] = user;
+}
