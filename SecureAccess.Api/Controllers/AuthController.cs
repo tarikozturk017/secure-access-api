8594e665494a118759449d360f56053834943306
tarikozturk017 <tarik.ozturkk1@gmail.com>
2026-01-20 20:28:46 -0500

    change to constructor injection in AuthController

--- SecureAccess.Api/Controllers/AuthController.cs
+++ SecureAccess.Api/Controllers/AuthController.cs
@@ -4,13 +4,19 @@ using SecureAccess.Api.Services;
 
 namespace SecureAccess.Api.Controllers;
 
+
 [ApiController]
 [Route("auth")]
 public sealed class AuthController : ControllerBase
 {
     private readonly IAuthService _auth;
+    private readonly TokenService _tokens;
 
-    public AuthController(IAuthService auth) => _auth = auth;
+    public AuthController(IAuthService auth, TokenService tokens)
+    {
+        _auth = auth;
+        _tokens = tokens;
+    }
 
     [HttpPost("register")]
     public ActionResult<AuthResponse> Register(RegisterRequest req)
@@ -31,12 +37,12 @@ public sealed class AuthController : ControllerBase
     }
 
     [HttpPost("login")]
-    public ActionResult<LoginResponse> Login(LoginRequest req, [FromServices] TokenService tokens)
+    public ActionResult<LoginResponse> Login(LoginRequest req)
     {
         try
         {
             var user = _auth.Login(req.Email, req.Password);
-            var token = tokens.CreateToken(user);
+            var token = _tokens.CreateToken(user);
             return Ok(new LoginResponse(user.Id, user.Email, token));
         }
         catch (UnauthorizedAccessException)
